47bb7beb2cb0a60c27c9263588a7883031202877
antonionicolaci <antonionicolaci48@gmail.com>
2023-06-19 17:55:22 +0200

    add Movie && Director class

--- index.php
+++ index.php
@@ -1,3 +1,46 @@
+<?php
+class Movie {
+    public $name;
+    public $producer;
+    public $prodComp;
+    public $filmLength;
+    public $director;
+    public $actor;
+    public $releaseDate;
+    public $state;
+    public $originalLanguage;
+    public $score;
+
+    function __construct(Director $director)
+    {
+        $this->director = $director;
+    }
+
+    function starScore ($score)
+    {
+        if ($score >= 1 || $score <= 2) {
+            return "★☆☆☆☆";
+        } elseif ($score >= 3 || $score <= 4) {
+            return "★★☆☆☆";
+        } elseif ($score >= 5 || $score <= 6) {
+            return "★★★☆☆";
+        } elseif ($score >= 7 || $score <= 8) {
+            return "★★★★☆";
+        } elseif ($score >= 9 || $score <= 10) {
+            return "★★★★★";
+        }
+    }
+};
+
+class Director {
+    public $name;
+    public $awards;
+    public $listFilm;
+    public $firstFeature;
+    public $dateBith;
+    public $age;
+}
+?>
 <!DOCTYPE html>
 <html lang="it">
 <head>
